26931a320ce10707b349692880ef22f832ff5d2c
tppkpqom <tppkp.qomaruddin@gmail.com>
2025-06-28 12:38:17 +0700

    update install tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
+// postcss.config.js
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    tailwindcss, // Ini akan menambahkan plugin Tailwind CSS
+    autoprefixer, // Ini akan menambahkan plugin Autoprefixer
+  ],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Pastikan base path adalah root. Jika deploy ke subfolder, misal 'my-app', gunakan '/my-app/'
+  build: {
+    outDir: 'dist', // Direktori output build
+  },
+});
